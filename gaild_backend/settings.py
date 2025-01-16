9336275d6fcfbc49305f0c2cccfa8825d831c5be
Anurag Pandey <anuragpandey9689@gmail.com>
2025-01-16 14:00:38 +0530

    Static files migration setting on collectstatic

--- gaild_backend/settings.py
+++ gaild_backend/settings.py
@@ -164,3 +164,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files moved here on collectstatic command
+STATIC_ROOT = "/var/www/gail-backend/static"
